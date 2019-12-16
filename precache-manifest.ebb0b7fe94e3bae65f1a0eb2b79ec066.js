8dc4f10501bcb5f0c0b97fa2fded42dcabba54c8
andrei.goman <andrey.goman@satellite-soft.ru>
2019-12-16 17:56:18 +0400

    initial commit

---
+++ precache-manifest.ebb0b7fe94e3bae65f1a0eb2b79ec066.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56ff08c91f8195cd805f1dd86623866f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1660f8076a7db4af6c3f",
+    "url": "/static/css/2.266626f9.chunk.css"
+  },
+  {
+    "revision": "6d04f872acf30f1e6510",
+    "url": "/static/css/main.d3b574fd.chunk.css"
+  },
+  {
+    "revision": "1660f8076a7db4af6c3f",
+    "url": "/static/js/2.83df9e1d.chunk.js"
+  },
+  {
+    "revision": "6d04f872acf30f1e6510",
+    "url": "/static/js/main.d87fc08e.chunk.js"
+  },
+  {
+    "revision": "9aabb23bcbd2c9e69d36",
+    "url": "/static/js/runtime-main.baeb1881.js"
+  }
+]);
